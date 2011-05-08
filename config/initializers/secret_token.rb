77b544cc00b4e694669d031cffef7d4d826dc6d9
alyosha <alyoshasun@gmail.com>
2011-05-08 15:03:38 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Isport::Application.config.secret_token = '68400a03b4cdee0d7dd489fedd8dee8f91d2a60d1eab4d8298d09144e560c381cadc0219fa599a15b198b6da5946ed7454b16261a686a2adfae34b850744bd29'
