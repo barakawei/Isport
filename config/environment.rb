d3bf3f27008432f17f2a2e7d869041e2f73f4273
qiuyuzhou <piggytommy@gmail.com>
2011-06-02 16:46:25 +0800

    整理css文件结构完成

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Isport::Application.initialize!
+
+Sass::Plugin.options[:template_location] = [['./public/stylesheets/sass', './public/stylesheets/css']]
