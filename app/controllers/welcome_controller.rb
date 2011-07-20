5dce105f3a9c9f39704829ce97d30f7ebe5bca6b
Ren Ye <magicstone03@gmail.com>
2011-07-21 00:28:54 +0800

    modify welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,12 @@ class WelcomeController < ApplicationController
   def index
     if current_user
       redirect_to home_path
-    else 
+    else
+      @citycount = City.count
+      @usercount = User.count
+      @groupcount = Group.count
+      @eventcount = Event.count
+
       render
     end
   end
