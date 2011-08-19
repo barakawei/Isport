0beed3a3cc04fd285d192e2bcfed39a95a5f2d0a
Ye Ren <magicstone03@gmail.com>
2011-08-19 17:35:22 +0800

    xx

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,22 @@
+class WelcomeController < ApplicationController
+  def index
+    if current_user
+      redirect_to home_path
+    elsif AppConfig[:registrations_closed]
+      redirect_to sign_in_path
+    else
+      @registe_wizard = true
+      @citycount = City.count
+      @usercount = User.count
+      @groupcount = Group.count
+      @eventcount = Event.count
+      @items = Item.order('rand()').limit(3)
+      @events = Event.where(:id => 1...10).order('rand()').limit(3)
+      @groups = Group.where(:id => 1...10).order('rand()').limit(3)
+      @people = Person.limit(40)
+
+      render
+    end
+  end
+
+end
