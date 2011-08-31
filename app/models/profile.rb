6338cc1fce554d67affe362df6b6fe35851a4d74
Ren Ye <magicstone03@gmail.com>
2011-09-01 01:51:52 +0800

    fix bugs of people profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,7 +4,6 @@ class Profile < ActiveRecord::Base
   accepts_nested_attributes_for :location
   before_validation :strip_and_downcase_name
   validates_presence_of :name
-  validates_format_of :name, :with => /[\w\s]+/
   validates_length_of :name, :maximum => 20
 
   def strip_and_downcase_name
