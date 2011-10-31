7be5e60043d302eba1c039d36e0bc712c6e4fb94
Ren Ye <magicstone03@gmail.com>
2011-10-31 15:29:02 +0800

    bug fix

--- app/models/item_topic_followship.rb
+++ app/models/item_topic_followship.rb
@@ -2,4 +2,5 @@ class ItemTopicFollowship < ActiveRecord::Base
   belongs_to :person
   belongs_to :item_topic, :counter_cache => :followers_count
 
+  validates_uniqueness_of :item_topic_id, :scope => :person_id
 end
